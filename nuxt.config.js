f5b04a078b66aafe10602e7057b23272d034b49a
Bryan R. Herman <bryan.herman@unnpp.gov>
2021-12-18 19:20:57 -0500

    add configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,23 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   docs: {
     primaryColor: '#E24F55',
-  }
+  },
+
+  head: {
+    title: 'nuxt',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: 'icon.png' }],
+  },
+
+  router: {
+    base: '/nuxtcontentdocstheme/'
+  },
 })
